5a513722e8b22301d2dc6d8d1d80d8fe477aa316
Dmitry <awagadro@mail.ru>
2019-05-10 07:06:58 +0300

    no message

--- src/students/web/HelloWorldServlet.java
+++ src/students/web/HelloWorldServlet.java
@@ -1,7 +1,6 @@
 package students.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,10 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloWorldServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/html;charset=utf-8");
-
-		PrintWriter pw = resp.getWriter();
-		pw.println("<H1>Hello, world! или Привет мир</H1>");
-
+		getServletContext().getRequestDispatcher("/hello.jsp").forward(req, resp);
 	}
 }
